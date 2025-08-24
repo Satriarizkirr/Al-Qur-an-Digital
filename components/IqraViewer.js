353b00bea68908ceefcca930b8070d380eb58637
Satria Rizki <rizkirohmat4@gmail.com>
2025-08-24 10:08:38 +0700

    Update IqraViewer.js

--- components/IqraViewer.js
+++ components/IqraViewer.js
@@ -1,8 +1,10 @@
+// File: components/IqraViewer.js
+
 import { Box, Spinner, VStack, Text, useColorMode } from "@chakra-ui/react";
 import { Document, Page, pdfjs } from "react-pdf";
 
-// Gunakan worker dari CDN agar aman di mobile
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+// Konfigurasi worker
+pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.min.js`;
 
 export default function IqraViewer({ file, currentPage, onDocumentLoadSuccess }) {
   const { colorMode } = useColorMode();
@@ -15,6 +17,8 @@ export default function IqraViewer({ file, currentPage, onDocumentLoadSuccess })
       borderRadius="lg"
       boxShadow="lg"
       overflow="hidden"
+      // PERBAIKAN: Beri tinggi minimal yang cukup besar untuk mencegah 'lompat'
+      // Nilai ini bisa disesuaikan, tapi 50vh (50% tinggi layar) biasanya aman
       minH={{ base: "50vh", md: "60vh" }}
       display="flex"
       justifyContent="center"
